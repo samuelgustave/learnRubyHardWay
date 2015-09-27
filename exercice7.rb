f4e50323e5c77cee44e6cd45a4972f3a605170ef
Samuel Gustave <gustavesam@gmail.com>
2015-09-27 16:14:10 -0500

    ex 7 and 8

---
+++ exercice7.rb
@@ -0,0 +1,21 @@
+puts "Mary had a little lamb." # print the text and line return
+puts "Its fleece was white as #{ 'snow' }." # print text and line return
+puts "And everywhere that Mary went." # print text and line return
+puts "." * 10 # print 10 periods
+
+end1 = "C" # assign string "C" to variable end1
+end2 = "h" # assign string "h" to variable end1
+end3 = "e" # assign string "e" to variable end1
+end4 = "e" # assign string "e" to variable end1
+end5 = "s" # assign string "s" to variable end1
+end6 = "e" # assign string "e" to variable end1
+end7 = "B" # assign string "B" to variable end1
+end8 = "u" # assign string "u" to variable end1
+end9 = "r" # assign string "r" to variable end1
+end10 = "g" # assign string "g" to variable end1
+end11 = "e" # assign string "e" to variable end1
+end12 = "r" # assign string "r" to variable end1
+
+# watch that print vs. puts on this line what's it do ?
+print end1 + end2 + end3 + end4 + end5 + end6 # prints "Cheese" without line return
+puts end7 + end8 + end9 + end10 + end11 + end12 # prints "Burger" with a line return
