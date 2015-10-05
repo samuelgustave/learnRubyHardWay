d9c950f6ba00eb56587d61dba8d8954c6796e80b
Samuel Gustave <gustavesam@gmail.com>
2015-10-04 22:33:45 -0500

    New exercise and correct name of files

---
+++ exercise10.rb
@@ -0,0 +1,18 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
+
+print "This is an escape sentence : "
+puts "\a \b \f \v "
